7aa8fe1254686c5a0930d8c83f9c7aa2ba0b1386
David Preil <dzpreil@gmail.com>
2021-09-02 17:04:33 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ClientUpdateForm from '/modules/clientUpdateForm/clientUpdateForm.js'
+import ClientUpdateForm from '/modules/clientUpdateForm'
 
 function App() {
   return (
